927e5ed8653333d183809d68d0e8ba5d80ff5e7d
RITIK <ritikdarbar17@gmail.com>
2024-12-08 11:03:53 +0530

    Update and rename positivenegative.java to PositiveNegativeChecker.java

---
+++ PositiveNegativeChecker.java
@@ -0,0 +1,19 @@
+  // Question 2 //
+  
+  import java.util.Scanner;
+
+public class PositiveNegativeChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        double number = scanner.nextDouble();
+
+        if (number > 0) {
+            System.out.println(number + " is a positive number.");
+        } else if (number < 0) {
+            System.out.println(number + " is a negative number.");
+        } else {
+            System.out.println(number + " is zero.");
+        }
+    }
+}
