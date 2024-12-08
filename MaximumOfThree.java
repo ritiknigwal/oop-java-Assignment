1cf12f0829897a1cf812d17ae10daf5c0e3b0865
RITIK <ritikdarbar17@gmail.com>
2024-12-08 14:44:52 +0530

    Update and rename MaxOfThree.java to MaximumOfThree.java

---
+++ MaximumOfThree.java
@@ -0,0 +1,28 @@
+  // Question 03 //
+
+import java.util.Scanner;
+
+public class MaximumOfThree {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        double num1 = scanner.nextDouble();
+        System.out.print("Enter second number: ");
+        double num2 = scanner.nextDouble();
+        System.out.print("Enter third number: ");
+        double num3 = scanner.nextDouble();
+
+        double max = findMaximum(num1, num2, num3);
+        System.out.println("Maximum of three numbers is: " + max);
+    }
+
+    public static double findMaximum(double num1, double num2, double num3) {
+        if (num1 >= num2 && num1 >= num3) {
+            return num1;
+        } else if (num2 >= num1 && num2 >= num3) {
+            return num2;
+        } else {
+            return num3;
+        }
+    }
+}
