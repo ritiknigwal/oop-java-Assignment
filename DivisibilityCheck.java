16d4e78035d8117985de449eddb6f8a72f278818
RITIK <ritikdarbar17@gmail.com>
2024-12-08 14:38:37 +0530

    Update DivisibilityCheck.java

--- DivisibilityCheck.java
+++ DivisibilityCheck.java
@@ -1,27 +1,23 @@
   // Ass 1 (que.8)  //
 
-  import java.util.Scanner;
+ import java.util.Scanner;
 
-  public class DivisibilityCheck {
-      public static void main(String[] args) {
-          Scanner scanner = new Scanner(System.in);
-  
-          System.out.print("Enter the number to check divisibility: ");
-          int number = scanner.nextInt();
-  
-          System.out.print("Enter the divisor: ");
-          int divisor = scanner.nextInt();
-  
-          if (divisor == 0) {
-              System.out.println("Error: Divisor cannot be zero.");
-          } else {
-              if (number % divisor == 0) {
-                  System.out.println(number + " is divisible by " + divisor);
-              } else {
-                  System.out.println(number + " is not divisible by " + divisor);
-              }
-          }
-  
-          scanner.close();
-      }
-  }
+public class DivisibilityCheck {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        double number = scanner.nextDouble();
+        System.out.print("Enter a divisor: ");
+        double divisor = scanner.nextDouble();
+
+        if (isDivisible(number, divisor)) {
+            System.out.println(number + " is divisible by " + divisor);
+        } else {
+            System.out.println(number + " is not divisible by " + divisor);
+        }
+    }
+
+    public static boolean isDivisible(double number, double divisor) {
+        return number % divisor == 0;
+    }
+}
